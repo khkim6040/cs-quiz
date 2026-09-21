d5c10d0534591905558485de1bb80b7291ad11d2
Gwanho Kim <khkim6040@gmail.com>
2026-09-21 21:50:45 +0900

    feat: SEO 메타데이터, robots.txt, sitemap.xml 추가

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_NAME = "CS Quiz";
+export const SITE_DESCRIPTION =
+  "매일 10문제로 컴퓨터 과학 핵심 개념을 익히는 한/영 CS 퀴즈. 자료구조, 알고리즘, 네트워크, 운영체제, 데이터베이스, 보안 등";
+export const SITE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || "https://cs-quiz.gwanho.com";
